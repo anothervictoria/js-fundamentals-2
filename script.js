647133592d683feed76fd8b6f24fbbcf1c78beca
anothervictoria <victoria.panidi@gmail.com>
2024-04-14 15:46:58 +0400

    js fundamentals 2

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict"; //should be the very first line of code. in other cases it won't be strict
+
+//strict mode lecture
+
+////////////////////////
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriverLicense = true;
+// if (hasDriversLicense) console.log("I can drive");
+
+function logger() {
+  console.log("My name is Jonas");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
