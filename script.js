8c603ec79497ae6eb7e9b95583d17cbbd23de86e
anothervictoria <victoria.panidi@gmail.com>
2024-04-15 13:52:11 +0400

    challenge about functions

--- script.js
+++ script.js
@@ -135,16 +135,16 @@ TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
 
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-// const scoreDolphins = calcAverage(44, 23, 71);
-// const scoreKoalas = calcAverage(65, 54, 49);
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
 
-const scoreDolphins = calcAverage(85, 54, 41);
-const scoreKoalas = calcAverage(23, 34, 27);
+// const scoreDolphins = calcAverage(85, 54, 41);
+// const scoreKoalas = calcAverage(23, 34, 27);
 
 function checkWinner(avgDolphins, avgKoalas) {
-  if (avgDolphins > avgKoalas) {
+  if (avgDolphins > 2 * avgKoalas) {
     console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
-  } else if (avgDolphins < avgKoalas) {
+  } else if (avgKoalas >= 2 * avgDolphins) {
     console.log(`Koalas win (${avgDolphins} vs. ${avgKoalas})`);
   } else {
     console.log("No team wins...");
